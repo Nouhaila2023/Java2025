9e67a1e03d12d96a69ab59fc68333441f6d117b6
Nouhaila2023 <nelh237@g.educaand.es>
2024-09-06 19:28:04 +0200

    Primer Commit

---
+++ Programacion/Tema1/Ejercicios/Ejercicio2.java
@@ -0,0 +1,15 @@
+package Ejercicios;
+
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        
+        int a = 3;
+        int b = 4;
+        int c = (2 * a * b);
+    
+        System.out.println("a = " + a + 2);
+        System.out.println("b = " + ((2 * a * b) + (- a)));
+        System.out.println("b = " + (c + (-a)));
+    }
+}
