cdae63e09c04f97d2ed1b56b82bafb5ef441cc62
Nouhaila2023 <nelh237@g.educaand.es>
2024-09-25 19:59:21 +0200

    Ejercicio1

---
+++ Programacion/Prueva/EjercicoNombre.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+//Ejercicio de clase
+//crea un programa que pida por teclado tu nombre y tus apellidos por separado
+//Tienes que mostrar poer pantalla , primero los apellidos luego una como y luego tu nombre
+
+
+public class EjercicoNombre {
+    public static void main(String[] args) {
+
+    String nombre;
+    String  primerApellido;
+    String segundoApellido;
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Cual es tu nombre: ");
+    nombre = sc.nextLine();
+    System.out.println("Cual es tu primer apellido: ");
+    primerApellido = sc.nextLine();
+    System.out.println("Cual es tu segundo apellido: ");
+    segundoApellido = sc.nextLine();
+
+    System.out.println("El nombre completo es: " + primerApellido + " " + segundoApellido + " , " + nombre );
+
+
+
+    }
+}
